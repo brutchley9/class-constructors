032143747a5bc5c1a8d7db6c92693c9027b52449
brutchley <bhutchison0901@yahoo.com>
2024-07-06 02:29:24 -0500

    first commit working with class constructors, models and classes

---
+++ assets/js/classes/Blonkis.js
@@ -0,0 +1,15 @@
+/*class Blonkis {
+    constructor(
+        name,
+        volume,
+        width,
+        depthOfNose
+    ) {
+        this.name = name;
+        this.volume = volume;
+        this.width = width;
+        this.depthOfNose = depthOfNose;
+    }
+};
+
+export default Blonkis;*/
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//import Blonkis from "./classes/Blonkis";
+
+console.log("Hello World!");
+//console.log(Blonkis.name);
+
+const rightNow = new Date()
+
+console.log("The current date and time is " + rightNow)
+console.log("The current date and time is " + rightNow.toDateString())
